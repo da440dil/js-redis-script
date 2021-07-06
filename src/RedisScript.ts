4c54367eaed88da12ed3a4542c50256145c08451
da440dil <da440dil@gmail.com>
2021-07-06 14:27:28 +0300

    rename redis key names param

--- src/RedisScript.ts
+++ src/RedisScript.ts
@@ -7,10 +7,10 @@ import { RedisClient } from 'redis';
 export class RedisScript<T> {
 	private client: RedisClient;
 	private src: string;
-	private keysNum: number;
+	private keyCount: number;
 	private hash: string;
 
-	constructor({ client, src, keysNum = 0 }: {
+	constructor({ client, src, keyCount = 0 }: {
 		/** Redis [client](https://github.com/NodeRedis/node-redis). */
 		client: RedisClient;
 		/** Redis [script](https://redis.io/commands/eval) to run. */
@@ -19,11 +19,11 @@ export class RedisScript<T> {
 		 * The number of arguments that represent Redis [key names](https://redis.io/commands/eval#introduction-to-eval).
 		 * By default equals 0.
 		 */
-		keysNum?: number;
+		keyCount?: number;
 	}) {
 		this.client = client;
 		this.src = src;
-		this.keysNum = keysNum;
+		this.keyCount = keyCount;
 		this.hash = createHash('sha1').update(src).digest('hex');
 	}
 	/**
@@ -32,12 +32,12 @@ export class RedisScript<T> {
 	 */
 	public run(...args: (string | number)[]): Promise<T> {
 		return new Promise((resolve, reject) => {
-			this.client.evalsha(this.hash, this.keysNum, ...args, (err, res) => {
+			this.client.evalsha(this.hash, this.keyCount, ...args, (err, res) => {
 				if (err) {
 					if (!(err instanceof Error && err.message.startsWith('NOSCRIPT '))) {
 						return reject(err);
 					}
-					return this.client.eval(this.src, this.keysNum, ...args, (err, res) => {
+					return this.client.eval(this.src, this.keyCount, ...args, (err, res) => {
 						if (err) {
 							return reject(err);
 						}
